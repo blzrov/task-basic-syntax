9ad80ddaf7a32b8f0c627560e0a6b9528c451b1c
Denis <molotov.ant@mail.ru>
2022-03-28 05:12:51 +0500

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,10 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    for (let i = 0; i < str.length; i++) {
+        const curr = roman[str[i]];
+        const next = roman[str[i + 1]];
+        curr < next ? (result -= curr) : (result += curr);
+    }
     return result;
 }
